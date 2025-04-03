e2c448d4ac8045f1613e936549b94678b64cc730
Katerina Ulicna <k.ulicna@seznam.cz>
2025-04-03 10:42:19 +0200

    start

---
+++ index.js
@@ -0,0 +1,36 @@
+const numbers = [
+    -24, -11, 27, 29, -4, -28, -21, -14, 3, -8, 24, 19, -25, -2, -1, 11, 32, -31, 5
+  ];
+
+ // 1. vypište všechna čísla do stránky
+ // document.body.innerHTML = `<p> ${numbers} </p>`;
+
+ // 2. druhá mocnina všech čísel 
+ // document.body.innerHTML += `<p>${numbers.map(num => num ** 2)}</p>`;
+ 
+ // 3. Vypište do stránky pouze záporná čísla.
+ // document.body.innerHTML += `<p>${numbers.filter(num => num < 0)}</p>`;
+
+ // 4. Vypište do stránky absolutní hodnotu všech čísel.
+ // document.body.innerHTML += `<p>${numbers.map(num => Math.abs(num))}</p>`;
+
+ // 5. Vypište do stránky pouze sudá čísla.
+ // document.body.innerHTML += `<p>${numbers.filter(num => num > 0)}</p>`;
+
+ // 6. Vypište do stránky pouze ta čísla, jejíchž absolutní hodnota je dělitelná třemi.
+ // document.body.innerHTML += `<p>${numbers.filter(num => Math.abs(num) % 3 === 0)}</p>`;
+
+ // 7. Vypište do stránky jak daleko je každé číslo v seznamu od čísla 5.
+ // document.body.innerHTML += `<p>${numbers.map(num => num - 5)}</p>`;
+
+ // 8. Vypište do stránky druhé mocnicny vzdáleností všech čísel od čísla 5.
+// document.body.innerHTML += `<p>${numbers.map(num => Math.abs(num - 5) ** 2)} </p>`;
+
+// 9. Spočítejte, kolik je v seznamu záporných čísel.
+// document. body.innerHTML += numbers.filter(num => num < 0).length;
+
+// 10. Spočítejte součet všech čísel v poli.
+document.body.innerHTML += 
+// 11. Spočítejte průměr všech čísel v poli.
+
+// 12. Spočítejte součet všech kladných čísel v poli.
